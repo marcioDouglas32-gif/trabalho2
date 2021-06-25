834d976e570ad7888f9bbbd8b4d684163b1d840a
Marcio Douglas <dmarcio998@gmail.com>
2021-06-24 23:35:43 -0300

    uhoh

--- crud/create.php
+++ crud/create.php
@@ -7,7 +7,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO white (Nome,Marca, Modelo, Mac) 
+  $sql = "INSERT INTO celular1 (Nome,Marca, Modelo, Mac) 
   VALUES ('douglas ', 'xiaomi', 'Redmi Note 9','58175481u26061uffnn')";
   // use exec() because no results are returne
   $conn->exec($sql);
